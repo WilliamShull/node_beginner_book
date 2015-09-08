8598f71e4d82d7222eaf1b9533cd578505a84088
will.shull@gmail.com <will.shull@gmail.com>
2015-09-08 14:34:51 -0700

    added in file upload and diplaying. included a png file in root

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,18 +1,19 @@
 var querystring = require('querystring');
 var fs = require('fs');
+var formidable = require('formidable');
 
-function start(response, postData) {
+function start(response) {
   console.log("Request handler 'start' was called");
 
   var body = '<html>' +
     '<head>' +
-    '<meta http=equiv="Content-Type" content="text/html; ' +
-    'charset=UTF=8" />' +
+    '<meta http-equiv="Content-Type" content="text/html; ' +
+    'charset=UTF-8" />' +
     '</head>' +
     '<body>' +
     '<form action="/upload" enctype="multipart/form-data" ' +
     'method="post">' +
-    '<input type="file" name="upload">' +
+    '<input type="file" name="upload" multiple="multiple">' +
     '<input type="submit" value="Upload file" />' +
     '</form>' +
     '</body>' +
@@ -23,12 +24,25 @@ function start(response, postData) {
   response.end();
 }
 
-function upload(response, postData) {
-  console.log("Request handler 'upload' was called");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("You've sent the text: " +
-  querystring.parse(postData).text);
-  response.end();
+function upload(response, request) {
+  console.log("Request handler 'upload' was called.");
+
+  var form = new formidable.IncomingForm();
+  console.log('about to parse');
+  form.parse(request, function(error, fields, files) {
+    console.log('parsing done');
+
+    fs.rename(files.upload.path, '/tmp/test.png', function(error) {
+      if (error) {
+        fs.unlink('/tmp/test.png');
+        fs.rename(files.upload.path, '/tmp/test.png');
+      }
+    });
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('received image:<br/>');
+    response.write("<img src='/show'/>");
+    response.end();
+  });
 }
 
 function show(response) {
