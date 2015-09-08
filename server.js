cf5ee56dbfc8907f972bff0bf9ef2148cdfda438
William Shull <will.shull@gmail.com>
2015-09-07 20:17:45 -0700

    page 50

--- server.js
+++ server.js
@@ -3,18 +3,23 @@ var url = require('url');
 
 function start(route, handle) {
   function onRequest(request, response) {
+    var postData = "";
     var pathname = url.parse(request.url).pathname;
     console.log('Request for ' + pathname + ' recieved.');
 
-    route(handle, pathname);
+    request.setEncoding("utf8");
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello world");
-    response.end();
+    request.addListener("data", function(postDataChunk) {
+      postData += postDataChunk;
+      console.log("Received POST data chunk '" + postDataChunk + "'.");
+    });
+
+    request.addListener("end", function() {
+      route(handle, pathname, response, postData);
+    });
   }
 
   http.createServer(onRequest).listen(8888);
-
   console.log('Server has started');
 }
 
